c16300d149336e5808acb43f91be6f3322c99481
Kev-1 <xolto@outlook.com>
2018-04-11 09:33:33 +0700

    added feature
    now can add subtract multiply and divide!

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,17 +1,38 @@
-//first java project
 import java.util.Scanner;
 public class MainFrame {
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		int myNumber;
-		myNumber = 5;
 		Scanner input = new Scanner(System.in);
-		System.out.println("Please input the first number:");
-		int one = input.nextInt();
-		System.out.println("Please input the second number:");
-		int two = input.nextInt();
-		int total = one + two;
-		System.out.println(one + "+" + two + "=" + total);
+		try {
+			System.out.println("Please input the first number:");
+			int one = input.nextInt();
+			System.out.println("Please input the second number:");
+			int two = input.nextInt();
+			System.out.println("Please select the calculator function");
+			String tompel = input.next();
+			if (tompel.equals("multiplication")) {
+				int total = one * two;
+				System.out.println(one + " Mutliplied by " + two + " Equals to " + total);
+			} else if (tompel.equals("addition")) {
+				int total = one + two;
+				System.out.println(one + " Added by " + two + " Equals to " + total);
+			} else if (tompel.equals("minus")) {
+				int total = one - two;
+				System.out.println(one + " Minus " + two + " Equals to " + total);
+			} else if (tompel.equals("divide")) {
+				int total = one / two;
+				System.out.println(one + " Divided by " + two + " Equals to " + total);
+			} else {
+				System.out.println("You have entered an invalid input.\nPlease try again.");
+				main(args);
+			}
+			} catch (java.util.InputMismatchException name) {
+		        System.out.println("You have entered a letter, or the number inputted exceeds 1 trillion.\nPlease try again.");
+		        main(args);
+			}
+		}
+	public static void addition() {
 		
 	}
 }
